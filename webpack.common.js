9d95ad2812f63434ab922d118df0e5354e3685ad
mattpe <mattpe@metropolia.fi>
2022-10-06 12:21:28 +0300

    update packages & webpack conf

--- webpack.common.js
+++ webpack.common.js
@@ -56,9 +56,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        type: 'asset/resource'
       }
     ]
   }
